e46946c091a9aaaa78188ee0b1a628ae08fae751
eve <touch@evetry.xyz>
2021-05-26 22:20:34 +0800

    finish cookbook axios btcprice | 完成axios的实战 ,下一步完善UI渲染到人能看的地步

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,20 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'About',
+}
+</script>export default {
+  name: 'Axios',
+  data:() => ({
+    info: 'ddddd',
+    }),
+}
+
+axios
+    .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then(response => {this.info = response.data.bpi})
+    .catch(error => console.log(error, "error"));
---
+++ src/views/Axios.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="btc">
+        <h1>Bitcoin Price Index</h1>
+        <p>{{info}}</p>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'Axios',
+  data(){
+      return {
+          info: 'x1',
+      }
+  },
+  methods: {
+      getBtcData(){
+        axios
+            .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            .then(response => {this.info = response.data.bpi})
+            .catch(error => console.log(error, "error"));
+      }
+  },
+  created (){
+      this.getBtcData()
+  }
+}
+
+
+</script>
